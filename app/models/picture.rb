45eaa988af09d67227d128ac9bd0733bb30f8843
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-08-28 13:36:41 +0900

    投稿者表示追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,10 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, presence: true
+  validates :image, presence: true
 
   has_many :favorites, dependent: :destroy
 
   belongs_to :user
-  has_many :favorites_users, through: :favorites, source: :user 
+  has_many :favorites_users, through: :favorites, source: :user
 end
